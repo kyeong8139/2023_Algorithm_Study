5307df6a7e3ace2379a944568a0a351905bfcddd
SeongHyeuk Park <95901686+goldenkiwi-hyeuk@users.noreply.github.com>
2024-08-30 07:19:05 +0900

    [Programmers-hyeuk] 124나라의숫자
    [Programmers-hyeuk] 124나라의숫자

---
+++ programmers/pg_124나라의숫자.java
@@ -0,0 +1,23 @@
+
+public class pg_124나라의숫자 {
+	class Solution {
+		public String solution(int n) {
+			String[] numbers = { "4", "1", "2" };
+			String answer = "";
+
+			int num = n;
+
+			while (num > 0) {
+				int remainder = num % 3;
+				num /= 3;
+
+				if (remainder == 0)
+					num--;
+
+				answer = numbers[remainder] + answer;
+			}
+
+			return answer;
+		}
+	}
+}
