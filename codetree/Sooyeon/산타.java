ff2376e62e72af84369efe85f72f26e8a3f2bf31
조수연 <melthetensai@gmail.com>
2024-09-09 22:37:46 +0900

    [baekjoon] 테트로미노_14500

--- codetree/Sooyeon/산타.java
+++ codetree/Sooyeon/산타.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class 산타 {
